b95383f88ad181494a479f2f373810c026f2abf0
Youtubers.yudan N.A <yudannuralif@gmail.com>
2024-03-01 10:39:16 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const bgAudio = document.getElementById('bgAudio');
+    const putarMusik = document.getElementById('putarMusik');
+
+    toggleAudio.addEventListener('click', function() {
+        if (bgAudio.paused) {
+            bgAudio.play();
+            toggleAudio.textContent = 'Pause Audio';
+        } else {
+            bgAudio.pause();
+            toggleAudio.textContent = 'Play Audio';
+        }
+    });
+});
